b3ba08d7b72d5ce56a9a0bcbcdc63dda18d177d7
DBug <frc3316@gmail.com>
2017-02-21 00:30:42 +0200

    Added yaw PID for drive distance

--- Config2017/src/Config.java
+++ Config2017/src/Config.java
@@ -2,307 +2,314 @@
 import java.util.Hashtable;
 
 public class Config {
-    public static Hashtable<String, Object> variablesB;
-    public static Hashtable<String, Object> constantsB;
+	public static Hashtable<String, Object> variablesB;
+	public static Hashtable<String, Object> constantsB;
 
-    public static Hashtable<String, Object> variablesA;
-    public static Hashtable<String, Object> constantsA;
+	public static Hashtable<String, Object> variablesA;
+	public static Hashtable<String, Object> constantsA;
 
-    private Config() {
-    }
-
-    static {
-	variablesB = new Hashtable<String, Object>();
-	constantsB = new Hashtable<String, Object>();
-
-	variablesA = new Hashtable<String, Object>();
-	constantsA = new Hashtable<String, Object>();
-
-	initConfig();
-	IO.initIO();
-    }
-
-    public static void addToConstantsA(String key, Object value) {
-	System.out.println("Trying to add to constants A: key " + key + " value " + value);
-
-	if (constantsA.containsKey(key)) {
-	    constantsA.replace(key, value);
-	} else {
-	    constantsA.put(key, value);
+	private Config() {
 	}
-    }
 
-    public static void addToVariablesA(String key, Object value) {
-	System.out.println("Trying to add to variables A: key " + key + " value " + value);
+	static {
+		variablesB = new Hashtable<String, Object>();
+		constantsB = new Hashtable<String, Object>();
 
-	if (variablesA.containsKey(key)) {
-	    variablesA.replace(key, value);
-	} else {
-	    variablesA.put(key, value);
+		variablesA = new Hashtable<String, Object>();
+		constantsA = new Hashtable<String, Object>();
+
+		initConfig();
+		IO.initIO();
 	}
-    }
 
-    public static void addToConstantsB(String key, Object value) {
-	System.out.println("Trying to add to constants B: key " + key + " value " + value);
+	public static void addToConstantsA(String key, Object value) {
+		System.out.println("Trying to add to constants A: key " + key + " value " + value);
 
-	if (constantsB.containsKey(key)) {
-	    constantsB.replace(key, value);
-	} else {
-	    constantsB.put(key, value);
-	}
-    }
-
-    public static void addToVariablesB(String key, Object value) {
-	System.out.println("Trying to add to variables B: key " + key + " value " + value);
-
-	if (variablesB.containsKey(key)) {
-	    variablesB.replace(key, value);
-	} else {
-	    variablesB.put(key, value);
-	}
-    }
-
-    public static void addToConstants(String key, Object value) {
-	addToConstantsA(key, value);
-	addToConstantsB(key, value);
-    }
-
-    public static void addToVariables(String key, Object value) {
-	addToVariablesA(key, value);
-	addToVariablesB(key, value);
-    }
-
-    /*
-     * NOTE: constants and variables that are common to both robot A and robot B
-     * should be added with addToConstants() or addToVariables()
-     * 
-     * Use different constants and variables for the two robots only if there is
-     * a difference. TestModeStuff
-     */
-    private static void initConfig() {
-	/*
-	 * Human IO
-	 */
-	{
-	    /*
-	     * Constants
-	     */
-	    {
-		/*
-		 * Joysticks
-		 */
-		{
-		    addToConstants("JOYSTICK_LEFT", 0);
-		    addToConstants("JOYSTICK_RIGHT", 1);
-		    addToConstants("JOYSTICK_OPERATOR", 2);
+		if (constantsA.containsKey(key)) {
+			constantsA.replace(key, value);
+		} else {
+			constantsA.put(key, value);
 		}
+	}
 
-		/*
-		 * Buttons
-		 */
-		{
-		    // Joystick operator
+	public static void addToVariablesA(String key, Object value) {
+		System.out.println("Trying to add to variables A: key " + key + " value " + value);
 
-		    addToVariables("button_Intake_Toggle", 5);
-
-		    addToVariables("button_Climbing_Toggle", 6);
-
-		    addToVariables("button_Chassis_Break_Toggle", 1);
-		    addToVariables("button_Chassis_LowerSpeed", 7);
-		    addToVariables("button_Chassis_HigherSpeed", 8);
+		if (variablesA.containsKey(key)) {
+			variablesA.replace(key, value);
+		} else {
+			variablesA.put(key, value);
 		}
-	    }
+	}
+
+	public static void addToConstantsB(String key, Object value) {
+		System.out.println("Trying to add to constants B: key " + key + " value " + value);
+
+		if (constantsB.containsKey(key)) {
+			constantsB.replace(key, value);
+		} else {
+			constantsB.put(key, value);
+		}
+	}
+
+	public static void addToVariablesB(String key, Object value) {
+		System.out.println("Trying to add to variables B: key " + key + " value " + value);
+
+		if (variablesB.containsKey(key)) {
+			variablesB.replace(key, value);
+		} else {
+			variablesB.put(key, value);
+		}
+	}
+
+	public static void addToConstants(String key, Object value) {
+		addToConstantsA(key, value);
+		addToConstantsB(key, value);
+	}
+
+	public static void addToVariables(String key, Object value) {
+		addToVariablesA(key, value);
+		addToVariablesB(key, value);
 	}
 
 	/*
-	 * RobotIO
+	 * NOTE: constants and variables that are common to both robot A and robot B
+	 * should be added with addToConstants() or addToVariables()
+	 * 
+	 * Use different constants and variables for the two robots only if there is
+	 * a difference. TestModeStuff
 	 */
-	{
-	    /*
-	     * Constants
-	     */
-	    addToConstants("CURRENT_CONTROL_COUNTER", 10);
+	private static void initConfig() {
+		/*
+		 * Human IO
+		 */
+		{
+			/*
+			 * Constants
+			 */
+			{
+				/*
+				 * Joysticks
+				 */
+				{
+					addToConstants("JOYSTICK_LEFT", 0);
+					addToConstants("JOYSTICK_RIGHT", 1);
+					addToConstants("JOYSTICK_OPERATOR", 2);
+				}
 
-	    {
+				/*
+				 * Buttons
+				 */
+				{
+					// Joystick operator
+
+					addToVariables("button_Intake_Toggle", 5);
+
+					addToVariables("button_Climbing_Toggle", 6);
+
+					addToVariables("button_Chassis_Break_Toggle", 1);
+					addToVariables("button_Chassis_LowerSpeed", 7);
+					addToVariables("button_Chassis_HigherSpeed", 8);
+				}
+			}
+		}
+
+		/*
+		 * RobotIO
+		 */
+		{
+			/*
+			 * Constants
+			 */
+			addToConstants("CURRENT_CONTROL_COUNTER", 10);
+
+			{
+				/*
+				 * Chassis
+				 */
+				addToConstantsA("CHASSIS_MOTOR_LEFT_REVERSE", false);
+				addToConstantsA("CHASSIS_MOTOR_RIGHT_REVERSE", true);
+
+				addToVariables("chassis_Joystick_Right_Axis", 1);
+				addToVariables("chassis_Joystick_Left_Axis", 5);
+
+				addToConstants("CHASSIS_LEFT_ENCODER_REVERSE", false);
+				addToConstants("CHASSIS_RIGHT_ENCODER_REVERSE", true);
+
+				addToConstants("CHASSIS_ENCODERS_DISTANCE_PER_PULSE", 0.00124224); // in
+				// meters
+
+				/*
+				 * Intake
+				 */
+				addToConstantsA("INTAKE_MOTOR_REVERSE", false);
+
+				/*
+				 * Climbing
+				 */
+				addToConstantsA("CLIMBING_MOTOR_REVERSE", false);
+			}
+		}
+
 		/*
 		 * Chassis
 		 */
-		addToConstantsA("CHASSIS_MOTOR_LEFT_REVERSE", false);
-		addToConstantsA("CHASSIS_MOTOR_RIGHT_REVERSE", true);
+		{
+			/*
+			 * Constants
+			 */
+			{
+			}
 
-		addToVariables("chassis_Joystick_Right_Axis", 1);
-		addToVariables("chassis_Joystick_Left_Axis", 5);
+			/*
+			 * Variables
+			 */
+			{
+				addToVariables("chassis_TankDrive_DeadBand", 0.05);
 
-		addToConstants("CHASSIS_LEFT_ENCODER_REVERSE", false);
-		addToConstants("CHASSIS_RIGHT_ENCODER_REVERSE", true);
+				addToVariables("chassis_SpeedFactor_Medium", -0.8);
+				addToVariables("chassis_SpeedFactor_Higher", -1.0);
+				addToVariables("chassis_SpeedFactor_Lower", -0.5);
 
-		addToConstants("CHASSIS_ENCODERS_DISTANCE_PER_PULSE", 0.00124224); // in
-										   // meters
+				addToVariables("chassis_TankDrive_InvertX", true);
+				addToVariables("chassis_TankDrive_InvertY", false);
+			}
+
+			/*
+			 * Drive Distance
+			 */
+			{
+				addToVariables("chassis_DriveDistance_PID_Tolerance", 0.025);
+
+				// PID
+				// BY CAMERA
+				addToVariables("chassis_DriveByCamera_PID_KP", 0.0);
+				addToVariables("chassis_DriveByCamera_PID_KI", 0.0);
+				addToVariables("chassis_DriveByCamera_PID_KD", 0.0);
+
+				// BY ENCODERS
+				{
+					// Right
+					addToVariables("chassis_DriveDistance_PID_RIGHT_KP", 0.0);
+					addToVariables("chassis_DriveDistance_PID_RIGHT_KI", 0.0);
+					addToVariables("chassis_DriveDistance_PID_RIGHT_KD", 0.0);
+					// Left
+					addToVariables("chassis_DriveDistance_PID_LEFT_KP", 0.0);
+					addToVariables("chassis_DriveDistance_PID_LEFT_KI", 0.0);
+					addToVariables("chassis_DriveDistance_PID_LEFT_KD", 0.0);
+					
+					// Yaw
+					addToVariables("chassis_DriveDistance_PID_YAW_KP", 50.0);
+					addToVariables("chassis_DriveDistance_PID_YAW_KI", 0.0);
+					addToVariables("chassis_DriveDistance_PID_YAW_KD", 0.0);
+					
+					{
+						// Long range
+
+						// Right
+						addToVariables("chassis_DriveDistanceLongRange_PID_RIGHT_KP", 125.0);
+						addToVariables("chassis_DriveDistanceLongRange_PID_RIGHT_KI", 1.5);
+						addToVariables("chassis_DriveDistanceLongRange_PID_RIGHT_KD", 3.0);
+						// Left
+						addToVariables("chassis_DriveDistanceLongRange_PID_LEFT_KP", 55.0);
+						addToVariables("chassis_DriveDistanceLongRange_PID_LEFT_KI", 1.5);
+						addToVariables("chassis_DriveDistanceLongRange_PID_LEFT_KD", 3.0);
+
+						// Short range
+
+						// Right
+						addToVariables("chassis_DriveDistanceShortRange_PID_RIGHT_KP", 850.0);
+						addToVariables("chassis_DriveDistanceShortRange_PID_RIGHT_KI", 1.0);
+						addToVariables("chassis_DriveDistanceShortRange_PID_RIGHT_KD", 0.0);
+						// Left
+						addToVariables("chassis_DriveDistanceShortRange_PID_LEFT_KP", 850.0);
+						addToVariables("chassis_DriveDistanceShortRange_PID_LEFT_KI", 1.0);
+						addToVariables("chassis_DriveDistanceShortRange_PID_LEFT_KD", 0.0);
+					}
+				}
+			}
+
+			/*
+			 * TURN BY GYRO
+			 */
+			{
+				// PID
+				addToVariables("chassis_TurnByGyro_PID_Tolerance", 1.0);
+
+				addToVariables("chassis_TurnByGyro_PID_KP", 75.0);
+				addToVariables("chassis_TurnByGyro_PID_KI", 0.7);
+				addToVariables("chassis_TurnByGyro_PID_KD", 5.0);
+			}
+
+			/*
+			 * Set Constant Voltage
+			 */
+			{
+				addToVariables("chassis_SetConstantVoltage_Voltage", 0.0);
+			}
+
+		}
 
 		/*
 		 * Intake
 		 */
-		addToConstantsA("INTAKE_MOTOR_REVERSE", false);
-		
+		{
+			/*
+			 * Constants
+			 */
+			{
+				addToConstants("INTAKE_SWITCH_THRESH", 100.0);
+			}
+
+			/*
+			 * Variables
+			 */
+			{
+				addToVariables("intake_MoveIntake_V", 1.0);
+				addToVariables("intake_RollIn_Voltage", 1.0);
+			}
+		}
+
+		/*
+		 * Installer
+		 */
+		{
+			/*
+			 * Constants
+			 */
+			{
+				addToConstants("INSTALLER_SWITCH_THRESH", 100.0);
+			}
+
+			/*
+			 * Variables
+			 */
+			{
+			}
+		}
+
 		/*
 		 * Climbing
 		 */
-		addToConstantsA("CLIMBING_MOTOR_REVERSE", false);
-	    }
-	}
-
-	/*
-	 * Chassis
-	 */
-	{
-	    /*
-	     * Constants
-	     */
-	    {
-	    }
-
-	    /*
-	     * Variables
-	     */
-	    {
-		addToVariables("chassis_TankDrive_DeadBand", 0.05);
-
-		addToVariables("chassis_SpeedFactor_Medium", -0.8);
-		addToVariables("chassis_SpeedFactor_Higher", -1.0);
-		addToVariables("chassis_SpeedFactor_Lower", -0.5);
-
-		addToVariables("chassis_TankDrive_InvertX", true);
-		addToVariables("chassis_TankDrive_InvertY", false);
-	    }
-
-	    /*
-	     * Drive Distance
-	     */
-	    {
-		addToVariables("chassis_DriveDistance_PID_Tolerance", 0.025);
-		
-		// PID
-		// BY CAMERA
-		addToVariables("chassis_DriveByCamera_PID_KP", 0.0);
-		addToVariables("chassis_DriveByCamera_PID_KI", 0.0);
-		addToVariables("chassis_DriveByCamera_PID_KD", 0.0);
-
-		// BY ENCODERS
 		{
-		// Right
-		addToVariables("chassis_DriveDistance_PID_RIGHT_KP", 0.0);
-		addToVariables("chassis_DriveDistance_PID_RIGHT_KI", 0.0);
-		addToVariables("chassis_DriveDistance_PID_RIGHT_KD", 0.0);
-		// Left
-		addToVariables("chassis_DriveDistance_PID_LEFT_KP", 0.0);
-		addToVariables("chassis_DriveDistance_PID_LEFT_KI", 0.0);
-		addToVariables("chassis_DriveDistance_PID_LEFT_KD", 0.0);
-		{
-    			// Long range
-		
-    			// Right
-            		addToVariables("chassis_DriveDistanceLongRange_PID_RIGHT_KP", 125.0);
-            		addToVariables("chassis_DriveDistanceLongRange_PID_RIGHT_KI", 1.5);
-            		addToVariables("chassis_DriveDistanceLongRange_PID_RIGHT_KD", 3.0);
-            		// Left
-            		addToVariables("chassis_DriveDistanceLongRange_PID_LEFT_KP", 55.0);
-            		addToVariables("chassis_DriveDistanceLongRange_PID_LEFT_KI", 1.5);
-            		addToVariables("chassis_DriveDistanceLongRange_PID_LEFT_KD", 3.0);
-            		
-            		// Short range
-            		
-    			// Right
-            		addToVariables("chassis_DriveDistanceShortRange_PID_RIGHT_KP", 850.0);
-            		addToVariables("chassis_DriveDistanceShortRange_PID_RIGHT_KI", 1.0);
-            		addToVariables("chassis_DriveDistanceShortRange_PID_RIGHT_KD", 0.0);
-            		// Left
-            		addToVariables("chassis_DriveDistanceShortRange_PID_LEFT_KP", 850.0);
-            		addToVariables("chassis_DriveDistanceShortRange_PID_LEFT_KI", 1.0);
-            		addToVariables("chassis_DriveDistanceShortRange_PID_LEFT_KD", 0.0);
+			/*
+			 * Constants
+			 */
+			{
+				addToConstants("CLIMBING_SWITCH_THRESH", 100.0); // SWITCH DOES
+				// NOT
+				// EXIST
+				addToConstants("CLIMBING_CURRENT_THRESH", 0.0); // TODO: Measure
+																// the current
+			}
+
+			/*
+			 * Variables
+			 */
+			{
+				addToVariables("climbing_Up_Voltage", 0.8);
+				addToVariables("climbing_Down_Voltage", -0.8);
+			}
 		}
-		}
-	    }
-
-	    /*
-	     * TURN BY GYRO
-	     */
-	    {
-		// PID
-		addToVariables("chassis_TurnByGyro_PID_Tolerance", 1.0);
-
-		addToVariables("chassis_TurnByGyro_PID_KP", 75.0);
-		addToVariables("chassis_TurnByGyro_PID_KI", 0.7);
-		addToVariables("chassis_TurnByGyro_PID_KD", 5.0);
-	    }
-
-	    /*
-	     * Set Constant Voltage
-	     */
-	    {
-		addToVariables("chassis_SetConstantVoltage_Voltage", 0.0);
-	    }
-
 	}
-
-	/*
-	 * Intake
-	 */
-	{
-	    /*
-	     * Constants
-	     */
-	    {
-		addToConstants("INTAKE_SWITCH_THRESH", 100.0);
-	    }
-
-	    /*
-	     * Variables
-	     */
-	    {
-		addToVariables("intake_MoveIntake_V", 1.0);
-		addToVariables("intake_RollIn_Voltage", 1.0);
-	    }
-	}
-
-	/*
-	 * Installer
-	 */
-	{
-	    /*
-	     * Constants
-	     */
-	    {
-		addToConstants("INSTALLER_SWITCH_THRESH", 100.0);
-	    }
-
-	    /*
-	     * Variables
-	     */
-	    {
-	    }
-	}
-
-	/*
-	 * Climbing
-	 */
-	{
-	    /*
-	     * Constants
-	     */
-	    {
-		addToConstants("CLIMBING_SWITCH_THRESH", 100.0); // SWITCH DOES
-								 // NOT
-								 // EXIST
-		addToConstants("CLIMBING_CURRENT_THRESH", 0.0); // TODO: Measure the current
-	    }
-
-	    /*
-	     * Variables
-	     */
-	    {
-		addToVariables("climbing_Up_Voltage", 0.8);
-		addToVariables("climbing_Down_Voltage", -0.8);
-	    }
-	}
-    }
 }
--- Robot Code/src/org/usfirst/frc/team3316/robot/auton/commands/DriveDistance.java
+++ Robot Code/src/org/usfirst/frc/team3316/robot/auton/commands/DriveDistance.java
@@ -19,15 +19,17 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
  */
 public class DriveDistance extends DBugCommand {
 
-	private double distRight, distLeft, initDistanceRight = 0, initDistanceLeft = 0;
-	private PIDController pidRight, pidLeft;
-	private double velocityRight = 0, velocityLeft = 0;
+	private double distRight, distLeft, initDistanceRight = 0, initDistanceLeft = 0, initYaw = 0;
+	private PIDController pidRight, pidLeft, pidYaw;
+	private double velocityRight = 0, velocityLeft = 0, ratio = 0;
 
 	public DriveDistance(double distanceRight, double distanceLeft) {
 		this.distRight = distanceRight;
 		this.distLeft = distanceLeft;
+		
 		initPIDRight();
 		initPIDLeft();
+		initPIDYaw();
 	}
 
 	private void initPIDRight() {
@@ -52,7 +54,7 @@ public class DriveDistance extends DBugCommand {
 			}
 		}, 0.02);
 	}
-	
+
 	private void initPIDLeft() {
 		pidLeft = new PIDController(0, 0, 0, new PIDSource() {
 			public void setPIDSourceType(PIDSourceType pidSource) {
@@ -71,7 +73,30 @@ public class DriveDistance extends DBugCommand {
 		}, new PIDOutput() {
 
 			public void pidWrite(double output) {
-			    velocityLeft = output;
+				velocityLeft = output;
+			}
+		}, 0.02);
+	}
+
+	private void initPIDYaw() {
+		pidYaw = new PIDController(0, 0, 0, new PIDSource() {
+			public void setPIDSourceType(PIDSourceType pidSource) {
+				return;
+			}
+
+			public double pidGet() {
+				double currentYaw = Robot.chassis.getYaw() - initYaw;
+
+				return currentYaw;
+			}
+
+			public PIDSourceType getPIDSourceType() {
+				return PIDSourceType.kDisplacement;
+			}
+		}, new PIDOutput() {
+
+			public void pidWrite(double output) {
+				ratio = output;
 			}
 		}, 0.02);
 	}
@@ -79,11 +104,10 @@ public class DriveDistance extends DBugCommand {
 	// Called just before this Command runs the first time
 	protected void init() {
 		Robot.chassis.setBrake(true);
-		
-		Robot.chassis.resetYaw();
-		
+
 		pidRight.setOutputRange(-1, 1);
 		pidLeft.setOutputRange(-1, 1);
+		pidYaw.setOutputRange(-1, 1);
 
 		pidRight.setAbsoluteTolerance((double) config.get("chassis_DriveDistance_PID_Tolerance"));
 		pidLeft.setAbsoluteTolerance((double) config.get("chassis_DriveDistance_PID_Tolerance"));
@@ -92,37 +116,46 @@ public class DriveDistance extends DBugCommand {
 				(double) config.get("chassis_DriveDistance_PID_RIGHT_KI") / 1000,
 				(double) config.get("chassis_DriveDistance_PID_RIGHT_KD") / 1000);
 		pidLeft.setPID((double) config.get("chassis_DriveDistance_PID_LEFT_KP") / 1000,
-			(double) config.get("chassis_DriveDistance_PID_LEFT_KI") / 1000,
-			(double) config.get("chassis_DriveDistance_PID_LEFT_KD") / 1000);
+				(double) config.get("chassis_DriveDistance_PID_LEFT_KI") / 1000,
+				(double) config.get("chassis_DriveDistance_PID_LEFT_KD") / 1000);
+		pidYaw.setPID((double) config.get("chassis_DriveDistance_PID_YAW_KP") / 1000,
+				(double) config.get("chassis_DriveDistance_PID_YAW_KI") / 1000,
+				(double) config.get("chassis_DriveDistance_PID_YAW_KD") / 1000);
 
 		pidRight.setSetpoint(distRight);
 		pidLeft.setSetpoint(distLeft);
+		pidYaw.setSetpoint(0.0);
 
 		initDistanceRight = Robot.chassis.getRightDistance();
 		initDistanceLeft = Robot.chassis.getLeftDistance();
+		initYaw = Robot.chassis.getYaw();
 
 		pidRight.enable();
 		pidLeft.enable();
+		pidYaw.enable();
 	}
 
 	// Called repeatedly when this Command is scheduled to run
 	protected void execute() {
-	    Robot.chassis.setMotors(velocityLeft, velocityRight);
+		Robot.chassis.setMotors(getLeftVolatge(velocityLeft, ratio), getRightVoltage(velocityRight, ratio));
 	}
 
 	// Make this return true when this Command no longer needs to run execute()
 	protected boolean isFinished() {
-	    return pidRight.onTarget() && pidLeft.onTarget();
+		return pidRight.onTarget() && pidLeft.onTarget();
 	}
 
 	// Called once after isFinished returns true
 	protected void fin() {
 		pidRight.reset();
 		pidRight.disable();
-		
+
 		pidLeft.reset();
 		pidLeft.disable();
 		
+		pidYaw.reset();
+		pidYaw.disable();
+
 		Robot.chassis.setMotors(0, 0);
 	}
 
@@ -130,5 +163,22 @@ public class DriveDistance extends DBugCommand {
 	// subsystems is scheduled to run
 	protected void interr() {
 		fin();
+	}
+
+	// Utils
+	private double getLeftVolatge(double v, double r) {
+		if (v > 0) {
+			return Math.max(v, v * (r + 1));
+		} else {
+			return Math.min(v, v * (r + 1));
+		}
+	}
+
+	private double getRightVoltage(double v, double r) {
+		if (v > 0) {
+			return Math.max(v, v * (-r + 1));
+		} else {
+			return Math.min(v, v * (-r + 1));
+		}
 	}
 }
